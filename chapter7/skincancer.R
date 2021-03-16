567402fa1f90d11140b76dc47b4476d1e0705d7e
bwarren <bwarren@cs.odu.edu>
2021-03-16 16:20:28 -0400

    three ch. 7 examples added

---
+++ chapter7/skincancer.R
@@ -0,0 +1,50 @@
+x <- skincancer$Lat
+y <- skincancer$Mort
+
+plot(x, y, xlab = "Lat",
+     ylab = "Mort", pch=16)
+
+cor(x,y)
+
+m <- lm(y ~ x)
+m
+
+abline(m, col = "red")
+
+new.x <- x - mean(x)
+lm(y ~ new.x)
+
+m1 <- lm(y ~ I(x - mean(x)))
+m1
+
+summary(m1)
+
+confint(m1, level = 0.95)
+
+predict(m, newdata = data.frame(x = 40), interval = "confidence")
+predict(m, newdata = data.frame(x = 40), interval = "confidence", level = 0.95)
+
+resid(m1)
+
+plot(x,resid(m1),ylab="Residuals",xlab="x", main="Residual plot for data in skincancer.txt", pch=16)
+abline(h=0) #adds a line through the x-axis
+
+#Difference between confidence and prediction intervals
+# Predict these data
+predx <- data.frame(x = seq(from = min(x), to = max(x), by = 0.1))
+
+# Confidence interval
+conf.int <- cbind(predx, predict(m, newdata = predx, interval = "confidence"))
+
+# Prediction interval
+pred.int <- cbind(predx, predict(m, newdata = predx, interval = "prediction"))
+
+# Graph
+plot(x, y, ylim = c(65, 105))
+abline(m, col = "red")
+lines(conf.int[,c(1,3)], col="blue", lty=2, lwd = 2)
+lines(conf.int[,c(1,4)], col="blue", lty=2, lwd = 2)
+lines(pred.int[,c(1,3)], col="green", lty=3, lwd = 2)
+lines(pred.int[,c(1,4)], col="green", lty=3, lwd = 2)
+legend("topleft", c("The least squares regression line", "Confidence band",
+                    "Prediction band"), col=c("red", "blue", "green"), lty=c(1:3), cex=1.3, lwd=2)
